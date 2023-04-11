fe289f921f8b9fc9c1ff286c7b666c713344ae48
askarmel <askarmel@gmail.com>
2023-04-11 22:11:18 +0800

    initialisation

---
+++ api/oanda_api.py
@@ -0,0 +1,28 @@
+import requests
+import constants.defs as defs
+
+class OandaApi:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers.update({
+            "Authorization": f"Bearer {defs.API_KEY}",
+            "Content-Type": "application/json"
+        })
+     
+    def make_request(self, url, verb='get', code=200, params=None, data=None, headers=None):
+        full_url = f"{defs.OANDA_URL}/{url}"
+        try:
+            response = None
+            if verb == 'get':
+                response = self.session.get(url, params=params, headers=headers)
+            if response == None:
+                return False, {'error': 'verb not found'}
+            
+            if response.status_code == code:
+                return True, response.json()
+            else:
+                return False, response.json()
+            
+        except Exception as error:
+            return False, {'Exception': error}
---
+++ main.py
@@ -0,0 +1,4 @@
+import api.oanda_api as oanda_api
+
+if __name__ == '__main__':
+    api = oanda_api.OandaApi()
